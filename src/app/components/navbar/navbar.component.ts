dd3867ff214ef931c07625c84d6a3ca6430b9797
Jormillo <jonaybrito15@gmail.com>
2021-07-25 20:21:43 +0200

    the css of the navbar has been changed and a test with git is going to be carried out

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,13 +7,32 @@ import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  result : string = new Date().toLocaleString();
+  diasSemana = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"]
+  fecha : Date = new Date();
+  diaDelMes : number = this.fecha.getDate();
+  diaDeLaSemana : number = this.fecha.getDay();
+  mes : number = this.fecha.getMonth();
+  año : number = this.fecha.getFullYear();
+  horas : number = this.fecha.getHours();
+  minutos : number = this.fecha.getMinutes();
+  segundos : number = this.fecha.getSeconds();
+  resultado = "";
 
-  constructor() {}
+  constructor() {
+    this.resultado = `${this.diasSemana[this.diaDeLaSemana]} ${this.diaDelMes} del ${this.mes + 1} de ${this.año}, hora: ${this.horas} : ${this.minutos} : ${this.segundos}`;
+  }
 
   ngOnInit(): void {
     setInterval(() => {
-      this.result = new Date().toLocaleString();
+      this.fecha = new Date();
+      this.diaDelMes = this.fecha.getDate();
+      this.diaDeLaSemana = this.fecha.getDay();
+      this.mes = this.fecha.getMonth();
+      this.año = this.fecha.getFullYear();
+      this.horas = this.fecha.getHours();
+      this.minutos = this.fecha.getMinutes();
+      this.segundos = this.fecha.getSeconds();
+      this.resultado = `${this.diasSemana[this.diaDeLaSemana]} ${this.diaDelMes} del ${this.mes + 1} de ${this.año}, hora: ${this.horas} : ${this.minutos} : ${this.segundos}`;
     }, 1000)
   } 
 
