59a65f8a6e28779d7070d2e020adf7a53cd859fe
Jormillo <jonaybrito15@gmail.com>
2021-08-18 19:44:43 +0200

    axios has been added and a request to the server testing the api

---
+++ src/app/services/empleados.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpleadosService {
+
+  constructor() {
+    // axios.get("http://localhost:3000/users")
+    // .then( users => {
+    //   console.log(users.data);
+      
+    // })
+  }
+}
