6058bea10b96656da64bdeac4dc2197a0f5eeba2
Jormillo <jonaybrito15@gmail.com>
2021-09-14 01:21:15 +0200

    it has been prepared so that when a user registers, the administrator is notified with an email to verify the registration.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsersService } from 'src/app/services/users/user.service';
-import { User } from 'src/app/utils/models.utils';
+import { EmailService } from 'src/app/services/email/email.service';
+import { Mail, User } from 'src/app/utils/models.utils';
 import * as CryptoJS from 'crypto-js';
 
 @Component({
@@ -16,7 +17,7 @@ export class RegisterComponent implements OnInit {
   clave = "clave"
   toDecript = "";
 
-  constructor(private userService : UsersService,  private router : Router) {
+  constructor(private userService : UsersService,  private router : Router, private emailService : EmailService) {
     // this.toDecript = this.encrypt(this.clave);
     // console.log(this.encrypt(this.clave));
     // console.log(this.decrypt(this.toDecript));
@@ -38,6 +39,16 @@ export class RegisterComponent implements OnInit {
     driver_number : ""
   }
 
+  mail : Mail = {
+    subject : "Se ha registrado un nuevo usuario.",
+    text : `Hola, \n
+    Un nuevo usuario se ha registrado y esta esperando que verifiques su registro. \n
+    Por favor, verífiquelo lo antes posible.\n
+    Atentamente desde la web del sindicato. \n
+    Saludos.
+    ` 
+  }
+
   postUser() {
     this.userService.postUser(this.user)
     .then(user => console.log(user));
@@ -47,6 +58,7 @@ export class RegisterComponent implements OnInit {
     this.postUser();
     // window.sessionStorage.setItem("loginEmail", this.user.email);
     // window.sessionStorage.setItem("loginPassword", this.user.password);
+    this.sendEmail(this.mail);
     this.router.navigateByUrl("/login").then(e => {
       if (e) {
         console.log("Navigation is successful!");
@@ -71,6 +83,11 @@ export class RegisterComponent implements OnInit {
 
   decrypt(textToDecrypt : string){
     return CryptoJS.AES.decrypt(textToDecrypt, this.palabraSecreta.trim()).toString(CryptoJS.enc.Utf8);
+  }
+
+  sendEmail(mail : Mail){
+    this.emailService.sendMail(mail);
+    alert("Se ha registrado correctamente.")
   }
 
 }
